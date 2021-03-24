4c2c01d61595ca69c161bdba7ed2fce8e3fe4e23
Sebastian Heikamp <sebastian.heikamp@promaterial.com>
2021-03-24 10:39:03 +0100

    Add login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.png';
 import './App.css';
+import Login from './Login';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Ligner, the place for competitive reading!
+          Welcome to Ligner!
         </p>
+        <Login />
       </header>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div>
+            <input placeholder="Enter your username here..."/>
+            <button>Login</button>
+        </div>
+    );
+}
+
+export default Login;
