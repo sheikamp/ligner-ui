117e95409b32c5310332e94a8b216ef03222b64a
Sebastian Heikamp <sebastian.heikamp@promaterial.com>
2021-03-25 16:58:58 +0100

    Add material-ui, style input and button for login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,24 @@
 import './Login.css'
 import {Link} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    input: {
+        background: "rgb(232, 241, 250)"
+    }
+}));
 
 const Login = () => {
+    const classes = useStyles();
     return (
         <>
             <div>
-                <input placeholder="Enter your username here..."/>
-                <button>Login</button>
+                <form>
+                    <TextField id="standard-basic" InputProps={{ className: classes.input }} />
+                    <Button variant="contained" color="primary" component={Link} to="/dashboard">Login</Button>
+                </form>
             </div>
             <p className="Sign-in-text">Not a member yet? Sign in <Link className="Sign-in-link" to="/signin">here.</Link></p>
         </>
